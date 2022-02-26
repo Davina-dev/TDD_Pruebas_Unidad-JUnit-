88e70f4d4d009ebc8bb21e13f092597c84fc0e7c
Davina-dev <davinamr91@gmail.com>
2022-02-26 15:38:49 +0100

    TEST failed:2 and passed:3

--- src/main/java/KatasDavina/BowlingGame.java
+++ src/main/java/KatasDavina/BowlingGame.java
@@ -1,13 +1,31 @@
 package KatasDavina;
 
 public class BowlingGame {
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
-    public Object score() {
-        return score;
-    }
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+
+        for( int frame = 0; frame<10; frame++) {
+
+            if (rolls[frameIndex] == 10){ //isStrike
+                score += 10 + rolls[frameIndex +1] + rolls[frameIndex + 2]; //strike bonus
+                frameIndex++;
+            }else if (rolls[frameIndex] + rolls[frameIndex + 1] == 10) {  // isSpare
+                score += 10 + rolls[frameIndex + 2]; //spareBonus
+                frameIndex += 2;
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1]; //sumOfBallsInFrame
+                frameIndex += 2;
+                }
+            }
+
+        return  score;
+        }
 }
--- src/test/java/KatasDavina/BowlingGameTest.java
+++ src/test/java/KatasDavina/BowlingGameTest.java
@@ -1,29 +1,75 @@
 /*https://www.youtube.com/watch?v=v_w7pSWeQiM*/
 package KatasDavina;
-
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class BowlingGameTest {
-/*miramos que  los 20 lanzamientos de 0 puntos la puntuación sea 0*/
+/*pod3emos crear UN OBJETO DE TIPO BOWLINGGAME EN UN METODO SETUP DE JUNIT (alt insertar -> setup method) así no lo he de repetir en cada test: BowlingGame bowlingGame = new BowlingGame();
+private BowlingGame bowlingGame;
+@Before
+public void setUp() throws Exception {
+bowlingGame= new BowlingGame(); }
+*/
+
+
+    /*miramos que  los 20 lanzamientos de 0 puntos la puntuación sea 0*/
         @Test
-        public void testGutter() throws Exception{
+        public void testGutter() {
             BowlingGame bowlingGame = new BowlingGame();
-            for (int i = 0; i<20; i++){
-                bowlingGame.roll(0);
-            }
+            rollMany(20,0);
             assertEquals(0, bowlingGame.score());
         }
 
         /*todos los lanzamientos tienen como punto : 1 */
     @Test
-    public void testAllOnes() throws Exception{
+    public void testAllOnes() {
         BowlingGame bowlingGame = new BowlingGame();
-        for (int i = 0; i<20; i++){
-            bowlingGame.roll(1);
-        }
+        rollMany(20,1);
         assertEquals(20, bowlingGame.score());
+    }
+    //funcionalidad de hacer la media. Consiste en derribar 10 bolos entre los dos lanzamientos de 1 turno
+    @Test
+    public void  testOneSpare(){
+
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, bowlingGame.score());
+    }
+
+
+// si derriba los 10 bolos en el 1er lanzamiento del turno
+    @Test
+    public void  testOneStrike(){
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(10);
+        bowlingGame.roll(3);
+        bowlingGame.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, bowlingGame.score());
+    }
+
+    @Test
+    public void testPerfectGame(){
+
+        BowlingGame bowlingGame = new BowlingGame();
+        rollMany(12,10);
+        assertEquals( 300, bowlingGame.score());
+
+    }
+
+
+   private void rollMany(int n, int pins) {
+        BowlingGame bowlingGame = new BowlingGame();
+        for (int i=0 ; i<n; i++){
+            bowlingGame.roll(pins);
+        }
+
     }
 
     }
